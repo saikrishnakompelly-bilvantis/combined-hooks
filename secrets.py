03ca17a62225864112af38369710c7006f66ef20
saikrishnakompelly-bilvantis <sai.kompellt@bilvantis.io>
2025-07-25 18:11:05 +0530

    sample

---
+++ secrets.py
@@ -0,0 +1,9 @@
+{
+  "type": "service_account",
+  "project_id": "fake-project-123",
+  "private_key_id": "abcdef1234567890",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhki...\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk@fake-project-123.iam.gserviceaccount.com",
+  "client_id": "123456789012345678901",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth"
+}
