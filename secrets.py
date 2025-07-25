e8322c5d1ba84bac5439bf7555b15363240c52d7
saikrishnakompelly-bilvantis <sai.kompellt@bilvantis.io>
2025-07-25 18:49:19 +0530

    sample

--- secrets.py
+++ secrets.py
@@ -16,3 +16,20 @@
   "client_id": "123456789012345678901",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth"
 }
+
+  "project_id": "fake-project-123",
+  "private_key_id": "abcdef1234567890",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhki...\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk@fake-project-123.iam.gserviceaccount.com",
+  "client_id": "123456789012345678901",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth"
+}
+{
+  "type": "service_account",
+  "project_id": "fake-project-123",
+  "private_key_id": "abcdef1234567890",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhki...\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk@fake-project-123.iam.gserviceaccount.com",
+  "client_id": "123456789012345678901",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth"
+}
